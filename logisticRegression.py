106fb31d90e864d2703d75e4da99cf8dfc24cb6a
dougcha <doug.cha@gmail.com>
2018-04-17 01:26:51 +0900

    initial success for gaga-classifier test

--- logisticRegression.py
+++ logisticRegression.py
@@ -6,37 +6,36 @@ import sympy.concrete.summations as sum
 from myutils import *
 from sklearn.utils import shuffle
 from mpmath import *
+import logging as log
 
 def setupTestData():
     df = pandas.read_csv('fakeGagaData.dat')
     return df
 
-# solver for logistic regression
-# psuedocode for now
-def grad_descent3(testData):
-    guesses = [1.0]*6    # initial guess for all 
+# generic solver takes in hypothesis function, cost func, training matrix, theta array, yarray
+def grad_descent4(hFunc, cFunc, trainingMatrix, thetaArr, yArr):
+    guesses = [1.0]*len(yArr)    # initial guess for all 
     step = 0.0001        # init step
     step_limit = 0.0001  # when to stop, when cost stops changing
     loop_limit = 5       # arbitrary max limits
     costChange = 1.0
 
     c,g,h,y,x = sp.symbols('c g h y x')
-    ts = sp.symbols('t0 t1 t2 t3 t4 t5')  #theta weight/parameter array
-    xs = sp.symbols('x0 x1 x2 x3 x4 x5')  #feature array
+    ts = sp.symbols('build thetaArr symbols')  #theta weight/parameter array
+    xs = sp.symbols('build x-featureArr symbols')  #feature array
 
-#    h = ts[0]*xs[0] + ts[1]*xs[1] + ts[2]*xs[2] + ts[3]*xs[3] + ts[4]*xs[4] + ts[5]*xs[5] 
-    h = ts[0]*xs[0] + ts[1]*xs[1]   # start w/ just 2 terms 
+    h = ts*xs
     g = 1 / (1+mp.e**-h)   # wrap h in sigmoid
     c = y*-sp.log(g) + (1-y)*-sp.log(1-g)  # cost func of single sample
     
-    print ('g: ',g)
-    print ('c: ',c)
+    log.info ('g: %s',g)
+    log.info ('c: %s',c)
 
-    print ('init guesses',guesses)
-    print ('init func: %s, test size: %d' %(str(g),testData.shape[0]))
+    log.info ('init guesses %s',str(guesses))
+    log.info ('init func: %s, test size: %d' %(str(g),testData.shape[0]))
     
     costF = evalSumF(c,xs,testData)  # cost fun evaluted for testData
-    print('init costF',str(costF)[:80]) # show first 80 char of cost evaluation
+    log.info('init costF',str(costF)[:80]) # show first 80 char of cost evaluation
     cost = 0.0+costF.subs(zip(ts,guesses))  
     print('init cost',cost,type(cost))
 
@@ -53,20 +52,66 @@ def grad_descent3(testData):
         i=i+1
     return guesses
 
+# solver for logistic regression
+# psuedocode for now
+def grad_descent3(testData):
+    step = 0.01          # init step
+    step_limit = 0.001   # when to stop, when cost stops changing
+    loop_limit = 50      # arbitrary max limits
+    costChange = 1.0
+
+    c,g,h,y,x = sp.symbols('c g h y x')
+#    ts = sp.symbols('t0 t1 t2 t3 t4 t5')  #theta weight/parameter array
+#    xs = sp.symbols('x0 x1 x2 x3 x4 x5')  #feature array
+    ts = sp.symbols('t0 t1 t2')  #theta weight/parameter array
+    xs = sp.symbols('x0 x1 x2')  #feature array
+    guesses = [0.01]*len(ts)    # initial guess for all 
+ 
+#    h = ts[0]*xs[0] + ts[1]*xs[1] + ts[2]*xs[2] + ts[3]*xs[3] + ts[4]*xs[4] + ts[5]*xs[5] 
+    h = ts[0]*xs[0] + ts[1]*xs[1] + ts[2]*xs[2]  # start w/ just few terms 
+    g = 1 / (1+mp.e**-h)   # wrap h in sigmoid
+    c = -y*sp.log(g) - (1-y)*sp.log(1-g)  # cost func of single sample
+    
+    log.info ('g: %s',g)
+    log.info ('c: %s',c)
+
+    log.info ('init guesses %s',guesses)
+    log.info ('init func: %s, test size: %d' %(str(g),testData.shape[0]))
+    
+    costF = evalSumF(c,xs,testData)  # cost fun evaluted for testData
+    log.info('init costF %s',str(costF)[:80]) # show first 80 char of cost evaluation
+    cost = 0.0+costF.subs(zip(ts,guesses))  
+    log.info('init cost %s ',cost)
+
+    i=0  
+    while (abs(costChange) > step_limit and i<loop_limit):  # arbitrary limiter
+        j=0
+        for theta in ts:
+            pd = evalPartialDeriv(c,theta,ts,xs,testData,guesses)
+            guesses[j] = guesses[j] - step * pd
+            j+=1
+        previousCost = cost
+        cost = costF.subs(zip(ts,guesses))
+        costChange = previousCost-cost
+        log.warn('i=%d,costChange=%f,cost=%f, guesses=%s'%(i, costChange,cost,str(guesses)))
+        i=i+1
+    return guesses
+
 # expnd to avg(sum(evaluated for testData)) 
 def evalSumF(f,xs,testData):  # @TODO change testData to matrix
     n=0.0
     for _,d in testData.iterrows():  # global test data
-        n += f.subs(xs[0],d.animal).subs(xs[1],d.vegetable).subs(sp.symbols('y'),d.gaga)   # TODO cleanup
+        n += f.subs(xs[0],d.animal).subs(xs[1],d.vegetable).subs(xs[2],d.anger).subs(sp.symbols('y'),d.gaga)   # TODO cleanup
+        log.info('------ expand: y:(%d) %s to %s ', d.gaga,str([d.animal,d.vegetable,d.anger]),str(n))
     n *= (1.0/len(testData))
-    print('f %s expanded (%d) n: %s '%(str(f),len(testData),str(n)))
+    log.info('f (%d) %s - \n  ->expanded: %s '%(len(testData), str(f), str(n)))
     return n 
 
 # generate deriv and sub all x's w/ training data and theta guess values
 def evalPartialDeriv(f,theta,ts,xs,testData,guesses):
     pdcost = evalSumF(sp.diff(f,theta),xs,testData)
     pdcost = pdcost.subs(zip(ts,guesses))
-    print ('pdcost f/t: ',pdcost,str(f),theta)
+    log.info ('    --> pdcost %f ;  %s  ;  %s: '%(pdcost,str(f),str(theta)))
     return pdcost
 
 def toMatrixTest(df):
@@ -93,7 +138,7 @@ def toSymbolsTest(df):
     print('g:',g)
 
     guesses = [1,1]
-    evalPartialDeriv(g, t[0], t[0:2], x[0:2], df, guesses)
+    evalPartialDeriv(g, t[0], t[0:3], x[0:3], df, guesses)
 
 # test runners
 def testLR():
@@ -101,7 +146,7 @@ def testLR():
     print df
     toMatrixTest(df)
     toSymbolsTest(df)
-    grad_descent3(df)
+    grad_descent3(df.head(10))
     print 'done'
 
 # store weights in theta[array] ?
@@ -110,5 +155,7 @@ def testLR():
 # use sympy ?  maybe to start
 # 
 
+log.basicConfig(level=log.WARN)
+log.info('start %s'%(log.getLogger().level))
 
 testLR()
