8fc0fdfd49fa1474a71d7abdc673f306d1fb3a3e
dougcha <doug.cha@gmail.com>
2018-05-06 23:07:14 +0900

    fix sample (divide py3 issue)s

--- tensorExamples.py
+++ tensorExamples.py
@@ -79,10 +79,50 @@ def test_linreg_tensor():
         print (theta_value)
     return theta_value
 
+def test_grad_tensor():
+    tf.reset_default_graph()
+    n_epochs = 400
+    learning_rate = 0.01
+
+    from sklearn.datasets import fetch_california_housing
+    housing = fetch_california_housing()  # auto-caches to /Users/dougfoo/scikit_learn_data/
+    m,n = housing.data.shape   # 20640,8 shape
+    housing_data_plus_bias = np.c_[np.ones((m,1)),housing.data]
+
+    from sklearn.preprocessing import StandardScaler
+    scaler = StandardScaler()
+    scaled_housing_data = scaler.fit_transform(housing.data)
+    scaled_housing_data_plus_bias = np.c_[np.ones((m, 1)), scaled_housing_data]
+
+    y = tf.constant(housing.target.reshape(-1,1), dtype=tf.float32, name='y')
+#    X = tf.constant(housing_data_plus_bias, dtype=tf.float32, name='X')   #otherwise need super small learning rate
+    X = tf.constant(scaled_housing_data_plus_bias, dtype=tf.float32, name="X")
+
+#    theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0, seed=42), name="theta")
+    theta = tf.Variable(tf.constant([[1.0],[1.0],[1.0],[1.0],[1.0],[1.0],[1.0],[1.0],[1.0]]), name='theta')
+    y_pred = tf.matmul(X, theta, name='predictions')
+    error = y_pred - y
+    mse = tf.reduce_mean(tf.square(error), name='mse')
+    gradients = 2.0/m * tf.matmul(tf.transpose(X), error)
+    training_op = tf.assign(theta, theta - learning_rate * gradients)   # what is this
+
+    init = tf.global_variables_initializer()
+
+    with tf.Session() as sess:
+        sess.run(init)
+        for epoch in range(n_epochs):
+            if (epoch % 25 == 0):
+                print('Epoch %s MSE %s'%(epoch, mse.eval()))
+            sess.run(training_op)   # whats an opp
+        best_theta = theta.eval()
+    print(best_theta)
+    return best_theta
+
 if __name__ == "__main__":
     log.getLogger().setLevel(log.INFO)
-    test_basic_tensor()
-    test_basic_tensor2()
-    test_basic_tensor3()
-    test_basic_tensor4()
-    test_linreg_tensor()
+    # test_basic_tensor()
+    # test_basic_tensor2()
+    # test_basic_tensor3()
+    # test_basic_tensor4()
+    # test_linreg_tensor()
+    test_grad_tensor()
