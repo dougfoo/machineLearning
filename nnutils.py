c7aff7a227dd2b904187d37f695f1deacf34e6d6
dougfoo <doug.cha@gmail.com>
2018-05-24 00:13:21 +0900

    removed eager

--- nnutils.py
+++ nnutils.py
@@ -89,20 +89,6 @@ def neuron_layer(X, n_neurons, name, activation=None):
         else:
                 return Z
 
-# alt eager eval builder
-def neuron_layer_eager(X, n_neurons, name, activation=None):
-    with tf.name_scope(name):
-        n_inputs = int(X.get_shape()[1])  # 1 or 0 - training rows
-        stddev = 2.0 / np.sqrt(n_inputs + n_neurons)
-        init = tf.truncated_normal((n_inputs, n_neurons), stddev=stddev)  # tensor n_input x n_neuron ?
-        W = tf.contrib.eager.Variable(init, name="kernel")  # tensor [MxN] ?
-        b = tf.contrib.eager.Variable(tf.zeros([n_neurons]),name='bias')  # row [1xN] ?
-        Z = tf.matmul(X,W) + b  # what is the shape result??  row [1xN] ?
-        if (activation is not None):
-            return activation(Z)
-        else:
-                return Z
-
 def relu(X):
     with tf.name_scope("relu"):
         with tf.variable_scope("relu", reuse=True):
