c7820e984675ccd1d7d355bda5933a5c59ba2e5b
dougcha <doug.cha@gmail.com>
2018-06-12 18:22:39 +0900

    broken nn

--- pytorchExamples.py
+++ pytorchExamples.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as tfun
 from myutils import *
+from nnutils import *
 from sklearn.utils import shuffle
 from torch.autograd import Variable
 import torchvision.models as models
@@ -292,42 +293,61 @@ def test_pytorch_nn():
     print ('input',test_input)
     print ('output/prediction',net.forward(test_input))   # returns activation
 
-# direct from tutorial
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        # 1 input image channel, 6 output channels, 5x5 square convolution
-        # kernel
-        self.conv1 = nn.Conv2d(1, 6, 5)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        # an affine operation: y = Wx + b
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+# apply higher level nn.Net lib to text
+def test_pytorch_nn_gaga():
+    net = GagaNet()
+    print(net)
 
-    # forward prop
-    def forward(self, x):
-        # Max pooling over a (2, 2) window
-        x = tfun.max_pool2d(tfun.relu(self.conv1(x)), (2, 2))
-        # If the size is a square you can only specify a single number
-        x = tfun.max_pool2d(tfun.relu(self.conv2(x)), 2)
-        x = x.view(-1, self.num_flat_features(x))  #purpose?
-        x = tfun.relu(self.fc1(x))
-        x = tfun.relu(self.fc2(x))
-        x = self.fc3(x)  # linear transform
-        return x
+    params = list(net.parameters())  
+    print(len(params))   # list of major params matrices
+    print(params[0].size())  # conv1's .weight
 
-    # whats purpose here
-    def num_flat_features(self, x):
-        size = x.size()[1:]  # all dimensions except the batch dimension
-        num_features = 1
-        for s in size:
-            num_features *= s
-        return num_features
+    input = torch.randn(1, 1, 500, 1)  # initial dummy input 500x1
+    output = net(input)
+    target = torch.tensor([[1.0]])  # expected 1 for 1 sample
+    criterion = nn.MSELoss()
+
+    loss = criterion(output, target)
+    print('loss tree',loss)
+    print(loss.grad_fn)  # MSELoss
+    print(loss.grad_fn.next_functions[0][0])  # Linear
+
+    # backprop
+    loss.backward()
+    print('net.inp.grad & net.hid.grad after backward')
+    print(net.inp.grad)
+    print(net.hid.grad)
+
+    # sample backprop otpmizer
+    import torch.optim as optim
+    optimizer = optim.SGD(net.parameters(), lr=0.01) # learning rate 0.01 / for each parameter matrix
+
+    # in your training loop:
+    for epoch in range(500):
+        optimizer.zero_grad()   # zero the gradient buffers
+        output = net(input)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()    # Does the update
+        if (epoch % 100 == 0):
+            print('Epoch %s MSE %s' % (epoch, loss))
+
+    print('net.inp.grad & net.hid.grad after GD loops')
+    print(net.inp.grad)
+    print(net.hid.grad)
+    print ('--training done---')
+
+    print('--test start---')
+    test_input = torch.randn(1, 1, 500, 1)  # initial dummy input
+    print ('input',test_input)
+    print ('output/prediction',net.forward(test_input))   # returns activation
+
+
 
 
 print ('--------------')
-test_pytorch_nn()
+test_pytorch_nn_gaga()
+#test_pytorch_nn()
 #print('--------------******-----------')
 #test_gaga_nn_auto()
 #test_gaga_lr()
