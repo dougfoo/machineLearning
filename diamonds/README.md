42723288f848f51c82d903ec371ed1ae99162bfa
dougfoo <doug.cha@gmail.com>
2019-09-10 19:11:53 -0400

    copy of diamond project from amarder

---
+++ diamonds/README.md
@@ -0,0 +1,3 @@
+Are you shopping for a diamond? Check out my blog post on [how to find great deals on Blue Nile][post].
+
+[post]: http://amarder.github.io/post/diamonds/
---
+++ diamonds/download2.py
@@ -0,0 +1,128 @@
+import json
+import pandas as pd
+import re
+import requests
+import argparse
+import warnings
+
+
+def _price_to_int(s):
+    if (isinstance(s,list)):
+        s = s[0]
+    return int(re.sub('[$,]', '', s))
+
+# It turns out Blue Nile's API will only let us grab 1000 diamonds
+# associated with each query. To get around this, let's use price to
+# page through the results.
+# 1. Get first thousand diamonds in query.
+# 2. Use diamond with highest price in that query to seed the next
+#    query.
+def diamonds(params):
+    assert params['sortColumn'] == 'price' and params['sortDirection'] == 'asc'
+
+    landing_page = requests.get('http://www.bluenile.com/')
+    url = 'https://www.bluenile.com/diamonds/round-cut?pt=setform&track=NavDiaSeaRD'
+    result = []
+    while True:
+        response = requests.get(url, params, cookies=landing_page.cookies)
+        d = json.loads(response.text)
+        print ('downloaded', response, response.url, d)
+        last_page = params['pageSize'] >= d['countRaw']
+
+        for i in range(len(d['results'])):
+            d['results'][i]['price'] = _price_to_int(d['results'][i]['price'])
+        max_price = d['results'][-1]['price']
+        min_price = d['results'][0]['price']
+
+        if last_page:
+            result += d['results']
+            break
+        else:
+            assert min_price < max_price, 'There are over %d diamonds with these characteristics at this price %d.' % (params['pageSize'], min_price)
+            result += [x for x in d['results'] if x['price'] < max_price]
+            params['minPrice'] = max_price
+    return result
+
+
+def clean(data):
+    # Put the data into a data frame.
+    df = pd.DataFrame(data)
+
+    # Clean up the data.
+    for col in ['carat', 'depth', 'lxwRatio', 'table']:
+        if (isinstance(col,list)):
+            col = col[0]
+        df[col] = df[col].map(lambda s: s.replace(',', '')).astype(float)
+    for col in ['pricePerCarat']:
+        if (isinstance(col,list)):
+            col = col[0]
+        df[col] = df[col].map(_price_to_int)
+
+    return df
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+
+    shapes = [
+        "RD",  # round
+        "PR",  # princess
+        "EC",  # emerald
+        "AS",  # asscher
+        "CU",  # cushion
+        "MQ",  # marquise
+        "RA",  # radiant
+        "OV",  # oval
+        "PS",  # pear
+        "HS",  # heart
+    ]
+    parser.add_argument('--shape', nargs='+', choices=shapes)
+
+    parser.add_argument('--minPrice', type=int)
+    parser.add_argument('--maxPrice', type=int)
+    parser.add_argument('--minCarat', type=float)
+    parser.add_argument('--maxCarat', type=float)
+
+    cuts = ['Good', 'Very Good', 'Ideal', 'Signature Ideal']
+    colors = ['J', 'I', 'H', 'G', 'F', 'E', 'D']
+    clarities = ['SI2', 'SI1', 'VS2', 'VS1', 'VVS2', 'VVS1', 'IF', 'FL']
+    select_one = [
+        ('minCut', cuts),
+        ('maxCut', cuts),
+        ('minColor', colors),
+        ('maxColor', colors),
+        ('minClarity', clarities),
+        ('maxClarity', clarities),
+    ]
+    for key, choices in select_one:
+        parser.add_argument('--%s' % key, choices=choices)
+
+    arguments_with_defaults = [
+        ('startIndex', 0),
+        ('pageSize', 1000),
+        ('country', 'USA'),
+        ('language', 'en-us'),
+        ('currency', 'USD'),
+        ('sortColumn', 'price'),
+        ('sortDirection', 'asc'),
+    ]
+    for k, v in arguments_with_defaults:
+        parser.add_argument('--%s' % k, default=v, type=type(v))
+
+    args = parser.parse_args()
+    d = args.__dict__
+    for k in list(d):
+        if d[k] is None:
+            del d[k]
+    return d
+
+
+def main():
+    params = parse_arguments()
+    l = diamonds(params)
+    df = clean(l)
+    print (df.head())
+    print (df.to_csv(index=False))
+
+if __name__ == '__main__':
+    main()
